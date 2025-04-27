7976abf1583352535251353e820d6868fa7f5007
AmaiOmikan <64887878+kg9n3n8y@users.noreply.github.com>
2025-04-27 20:01:04 +0900

    Add files via upload

---
+++ fudalist.js
@@ -0,0 +1,16 @@
+const fudalist = [
+    // む
+    { no:  87, kimariji: "む", normal: "./torifuda/tori_mu.png", reverse: "./torifuda/tori_r_mu.png"},
+    // す
+    { no:  18, kimariji: "す", normal: "./torifuda/tori_su.png", reverse: "./torifuda/tori_r_su.png"},
+    // め
+    { no:  57, kimariji: "め", normal: "./torifuda/tori_me.png", reverse: "./torifuda/tori_r_me.png"},
+    // ふ
+    { no:  22, kimariji: "ふ", normal: "./torifuda/tori_fu.png", reverse: "./torifuda/tori_r_fu.png"},
+    // さ
+    { no:  70, kimariji: "さ", normal: "./torifuda/tori_sa.png", reverse: "./torifuda/tori_r_sa.png"},
+    // ほ
+    { no:  81, kimariji: "ほ", normal: "./torifuda/tori_ho.png", reverse: "./torifuda/tori_r_ho.png"},
+    // せ
+    { no:  77, kimariji: "せ", normal: "./torifuda/tori_se.png", reverse: "./torifuda/tori_r_se.png"}
+]
